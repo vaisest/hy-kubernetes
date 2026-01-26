110d50edb6b19c9cd4236bcd61b8af982818c60c
vaisest <4550061+vaisest@users.noreply.github.com>
2026-01-26 20:46:26 +0200

    1.5

--- todo-app/main.py
+++ todo-app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def do_something():
-    print("hello from /")
+@app.get("/", response_class=HTMLResponse)
+async def do_something():
+    return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>HTML!</h1>
+        </body>
+    </html>
+    """
