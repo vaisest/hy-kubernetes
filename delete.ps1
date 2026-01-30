3416cd3d7badfe23ee01575a683c6d12cd19dfb2
vaisest <4550061+vaisest@users.noreply.github.com>
2026-01-30 21:18:50 +0200

    test

--- delete.ps1
+++ delete.ps1
@@ -1,2 +1,2 @@
 gcloud container clusters delete dwk-cluster --zone=europe-north1-b
-gcloud container clusters create dwk-cluster --zone=europe-north1-b --cluster-version=1.32 --disk-size=32 --num-nodes=3 --machine-type=e2-medium
+gcloud container clusters create dwk-cluster --zone=europe-north1-b --cluster-version=1.32 --disk-size=32 --num-nodes=3 --machine-type=e2-medium 
